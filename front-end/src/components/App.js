34c8ea66a0c0b2b6d2390b4f1875068743fdedb8
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-10-17 01:03:01 -0300

    fix: correcting api url

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -7,7 +7,7 @@ function App() {
   useEffect(async () => {
     const API_URL = process.env.REACT_APP_BASE_URL;
     try {
-      const response = await axios.get(`${API_URL}`);
+      const response = await axios.get(`${API_URL}/students/random`);
       const student = response.data;
       if (!student) {
         alert("Xablau! Não há estudantes cadastrados para o sorteio!");
